a2dd0979aa335e7d78b84f447831703b7388c693
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-04-05 19:31:44 +0100

    WIP ensure shopper fields encrypted

--- SuperSillyStickerz/settings.py
+++ SuperSillyStickerz/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stickerz',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- stickerz/admin.py
+++ stickerz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from stickerz.models import Shopper, Sticker
 
 # Register your models here.
+class ShopperAdmin(admin.ModelAdmin):
+    list_display = ('user', 'shippingFName', 'shippingLName', 'shippingAddress', 'shippingCountry', 'billingFName', 'cardNo' )
+
+admin.site.register(Shopper, ShopperAdmin)
