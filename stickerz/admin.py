60deb170df7548a9d88f6d38c21d63a380891970
Bella <2770706h@student.gla.ac.uk>
2024-04-06 12:47:00 +0100

    WIP encryption fixed everything i broke in the last 2 commits

--- stickerz/admin.py
+++ stickerz/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from stickerz.models import Shopper, Sticker
 
-admin.site.register(Shopper)
+class ShopperAdmin(admin.ModelAdmin):
+    list_display = ('user', 'shippingFName', 'shippingLName', 'shippingAddress', 'shippingCountry', 'billingFName', 'cardNo' )
+    
 admin.site.register(Sticker)
--- stickerz/models.py
+++ stickerz/models.py
@@ -39,28 +39,27 @@ class Shopper(models.Model):
     billingAddress = models.CharField(max_length=512)
     billingCountry = models.CharField(max_length=64)
     billingPostcode = models.CharField(max_length=8)
-    cardNo = models.CharField(max_length=12)
-    expiration = models.DateField()
+    cardNo = models.CharField(max_length=16)
+    expiration = models.CharField(max_length=8)
     cvv = models.CharField(max_length=3)
 
     def __str__(self):
         return self.shippingFName
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if not self.pk and not self.user:
-            self.shippingFName = make_password(self.shippingFName, True)
-            self.shippingLName = make_password(self.shippingFName, True)
-            self.shippingAddress = make_password(self.shippingAddress, True)
-            self.shippingCountry = make_password(self.shippingCountry, True)
-            self.shippingPostcode = make_password(self.shippingPostcode, True)
+        self.shippingFName = make_password(self.shippingFName)
+        self.shippingLName = make_password(self.shippingFName)
+        self.shippingAddress = make_password(self.shippingAddress)
+        self.shippingCountry = make_password(self.shippingCountry)
+        self.shippingPostcode = make_password(self.shippingPostcode)
 
-            self.billingFName = make_password(self.billingFName, True)
-            self.billingLName = make_password(self.billingLName, True)
-            self.billingAddress = make_password(self.billingAddress, True)
-            self.billingCountry = make_password(self.billingCountry, True)
-            self.billingPostcode = make_password(self.billingPostcode, True)
-            self.cardNo = make_password(self.cardNo, True)
-            self.expiration = make_password(self.expiration, True)
-            self.cvv = make_password(self.cvv, True)
-            self.save()
+        self.billingFName = make_password(self.billingFName)
+        self.billingLName = make_password(self.billingLName)
+        self.billingAddress = make_password(self.billingAddress)
+        self.billingCountry = make_password(self.billingCountry)
+        self.billingPostcode = make_password(self.billingPostcode)
+        self.cardNo = make_password(self.cardNo)
+        self.expiration = make_password(self.expiration)
+        self.cvv = make_password(self.cvv)
+
+        super(Shopper, self).save(*args, **kwargs)
