e0d45591c188d450997b24389ddae24b27db88c9
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-04-10 13:26:59 +0100

    Complete population script

--- populate_stickerz.py
+++ populate_stickerz.py
@@ -6,274 +6,335 @@ import django
 django.setup()
 from stickerz.models import Shopper, Sticker, Order
 from django.contrib.auth.models import User
+from SuperSillyStickerz.urls import static
 import decimal
 import dateutil.parser
 
 def populate():
     populate_stickers()
-    populate_users()
-    populate_orders()
+    # populate_users()
+    # populate_orders()
 
 def populate_stickers():
     catStickers = [
         {
             'name': 'Window Cat',
             'price': "1.20",
+            'image': 'window_cat.png'
         },
         {
             'name': 'Meowgical Cat',
             'price': "1.05",
+            'image': 'meowgical.jpeg'
         },
         {
             'name': 'Space Cat',
             'price': "1.15",
+            'image': 'space_cat.png'
         },
         {
             'name': 'Catstronaut',
             'price': "1.00",
+            'image': 'catstronaut.jpg'
         },
         {
             'name': 'Sneaky Black Cat',
             'price': "1.10",
+            'image': 'sneaky_black_cat.jpg'
         },
         {
             'name': 'Mermaid Cat',
             'price': "1.20",
+            'image': 'mermaid_cat.jpg'
         },
     ]
     peopleStickers = [
         {
             'name': 'Matt',
             'price': 0.50,
+            'image': 'matt.jpg'
         },
         {
             'name': 'Derek',
             'price': 0.50,
+            'image': 'derek.jpeg'
         },
         {
             'name': 'David Tennant',
             'price': 1.00,
+            'image': 'david_tennant.jpg'
         },
         {
             'name': 'Spock',
             'price': 1.15,
+            'image': 'spock.jpg'
         },
         {
             'name': 'Einstein',
             'price': 1.25,
+            'image': 'einstein.jpg'
         },
         {
             'name': 'Marilyn Monroe',
             'price': 1.10,
+            'image': 'marilyn_monroe.jpg'
         },
     ]
     dogStickers = [
         {
             'name': 'Sausage Dog',
             'price': 1.20,
+            'image': 'sausage_dog.jpg'
         },
         {
             'name': 'Heart Dog',
             'price': 1.05,
+            'image': 'heart_dog.jpg'
         },
         {
             'name': 'Funny Heart',
             'price': 1.00,
+            'image': 'funny_dog.jpg'
         },
         {
             'name': 'Carrot Dog',
             'price': 0.95,
+            'image': 'carrot_dog.jpg'
         },
         {
             'name': 'Happy Dog',
             'price': 0.50,
+            'image': 'happy_dog.jpg'
         },
         {
             'name': 'Pawficer',
             'price': 0.90,
+            'image': 'pawficer.jpg'
         },
     ]
     placeStickers = [
         {
             'name': 'Paris',
             'price': 1.00,
+            'image': 'paris.jpg'
         },
         {
             'name': 'London',
             'price': 1.00,
+            'image': 'london.jpg'
         },
         {
             'name': 'New York',
             'price': 1.15,
+            'image': 'new_york.jpg'
         },
         {
             'name': 'Manila',
             'price': 1.00,
+            'image': 'manila.jpg'
         },
         {
             'name': 'Berlin',
             'price': 1.20,
+            'image': 'berlin.jpg'
         },
         {
             'name': 'Madrid',
             'price': 1.25,
+            'image': 'madrid.jpg'
         },
     ]
     techStickers = [
         {
             'name': 'Tux',
             'price': 0.75,
+            'image': 'tux.jpg'
         },
         {
             'name': 'Women In Tech',
             'price': 1.00,
+            'image': 'women_in_tech.jpg'
         },
         {
             'name': 'Dont Forget To Save',
             'price': 0.50,
+            'image': 'dont_forget_to_save.jpg'
         },
         {
             'name': 'Java Developer',
             'price': 1.00,
+            'image': 'java_developer.jpg'
         },
         {
             'name': 'Heartbeat Python',
             'price': 0.20,
+            'image': 'heartbeat_python.jpg'
         },
         {
             'name': 'Tech Brain',
             'price': 1.15,
+            'image': 'tech_brain.jpg'
         },
     ]
     fashionStickers = [
         {
             'name': 'Handbag',
             'price': 0.60,
+            'image': 'handbag.jpg'
         },
         {
             'name': 'Red Bottom Heels',
             'price': 1.00,
+            'image': 'red_bottom_heels.jpg'
         },
         {
             'name': 'Shoe Addict',
             'price': 0.50,
+            'image': 'shoe_addict.jpg'
         },
         {
             'name': 'I Love Shopping',
             'price': 1.00,
+            'image': 'i_love_shopping.jpg'
         },
         {
             'name': 'Retail Therapy',
             'price': 0.80,
+            'image': 'retail_therapy.jpg'
         },
         {
             'name': 'Fashion Girl',
             'price': 1.20,
+            'image': 'fashion_girl.jpg'
         },
     ]
     cuteStickers = [
         {
             'name': 'I Love You Heart',
             'price': 1.32,
+            'image': 'i_love_you_heart.jpg'
         },
         {
             'name': 'Axolotl Milk',
             'price': 1.00,
+            'image': 'axolotl_milk.jpg'
         },
         {
             'name': 'Cute Cactus',
             'price': 1.10,
+            'image': 'cute_cactus.jpg'
         },
         {
             'name': 'Strawberry Frog',
             'price': 1.00,
+            'image': 'strawberry_frog.jpg'
         },
         {
             'name': 'Cute Unicorn',
             'price': 0.80,
+            'image': 'cute_unicorn.jpg'
         },
         {
             'name': 'Cute Watermelon',
             'price': 1.15,
+            'image': 'cute_watermelon.jpg'
         },
     ]    
     skatingStickers = [
         {
             'name': 'I Love To Skate',
             'price': 1.30,
+            'image': 'i_love_to_skate.jpg'
         },
         {
             'name': 'Skate',
             'price': 1.00,
+            'image': 'skate.jpg'
         },
         {
             'name': 'Skateboard Heart',
             'price': 0.95,
+            'image': 'skateboard_heart.jpg'
         },
         {
             'name': 'This Is How I Roll',
             'price': 1.22,
+            'image': 'this_is_how_i_roll.jpg'
         },
         {
             'name': 'Cool Girls Skate',
             'price': 0.84,
+            'image': 'cool_girls_skate.jpg'
         },
         {
             'name': 'Pizza Skateboard',
             'price': 1.15,
+            'image': 'pizza_skateboard.png'
         },
     ]
     brandStickers = [
         {
             'name': 'Nutalla',
             'price': 1.00,
+            'image': 'nutella.jpg'
         },
         {
             'name': 'Kappa',
             'price': 1.20,
+            'image': 'kappa.jpg'
         },
         {
             'name': 'Jim Beam',
             'price': 1.35,
+            'image': 'jim_beam.jpg'
         },
         {
             'name': 'Vans',
             'price': 1.24,
+            'image': 'vans.jpg'
         },
         {
             'name': 'Kool Aid',
             'price': 0.60,
+            'image': 'kool_aid.jpg'
         },
         {
             'name': 'Playstation',
             'price': 1.12,
+            'image': 'playstation.jpg'
         },
     ]
     schoolStickers = [
         {
             'name': 'School Is Fun',
             'price': 0.30,
+            'image': 'school_is_fun.png'
         },
         {
             'name': 'Graduation',
             'image': 9,
             'price': 1.00,
+            'image': 'graduation.jpg'
         },
         {
             'name': 'Too Cool For School',
             'price': 1.43,
+            'image': 'too_cool_for_school.jpg'
         },
         {
             'name': 'Welcome Back To School',
             'price': 1.20,
+            'image': 'welcome_back_to_school.jpg'
         },
         {
             'name': 'Back To School',
             'price': 0.75,
+            'image': 'back_to_school.png'
         },
         {
             'name': 'Pencil',
             'price': 0.85,
+            'image': 'pencil.jpg'
         },
     ]
     stickers = {'Cats': {'items': catStickers},
@@ -286,13 +347,14 @@ def populate_stickers():
             'Cute': {'items': cuteStickers},
             'Brands': {'items': brandStickers},
             'School': {'items': schoolStickers}}
+
     
     for stickercat, sticker_data in stickers.items():
         print(stickercat)
         for item in sticker_data['items']:
-            add_sticker(stickercat, item['name'], item['price'], "Matte")
-            add_sticker(stickercat, item['name'], item['price'], "Gloss")
-            add_sticker(stickercat, item['name'], item['price'], "Holo")
+            add_sticker(stickercat, item['name'], item['price'], "Matte", item['image'])
+            add_sticker(stickercat, item['name'], item['price'], "Gloss", item['image'])
+            add_sticker(stickercat, item['name'], item['price'], "Holo", item['image'])
 
 def populate_users():
     userOne = {
@@ -481,9 +543,10 @@ def populate_orders():
         shopper = data['username']
         add_order(shopper, data)
 
-def add_sticker(stickerCat, stickerName, stickerPrice, stickerFinish):
+def add_sticker(stickerCat, stickerName, stickerPrice, stickerFinish, stickerImage):
     stickerPrice = decimal.Decimal(float(stickerPrice))
-    sticker = Sticker.objects.get_or_create(name=stickerName, category=stickerCat, price=stickerPrice, finish=stickerFinish)[0]
+    image = os.path.join('/sticker_images', stickerImage)
+    sticker = Sticker.objects.get_or_create(name=stickerName, category=stickerCat, price=stickerPrice, finish=stickerFinish, image=image)[0]
     sticker.save()
     return sticker
 
