fe31a59e8e29305e06bfbe92f39e326d8b6deb2e
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-04-15 19:55:41 +0100

    shopper has first and last name

--- stickerz/forms.py
+++ stickerz/forms.py
@@ -13,7 +13,7 @@ class RegisterForm(forms.ModelForm):
  
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('first_name', 'last_name','username', 'email', 'password')
 
 
 
