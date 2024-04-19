35a24b5c90e9f0fb24cf058145569db3e0adf37a
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-04-19 11:36:45 +0100

    Add test descriptions

--- tests/views_test.py
+++ tests/views_test.py
@@ -1,21 +1,26 @@
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from stickerz.models import Sticker
-from django.contrib.auth.models import AnonymousUser
-
+from django.test.client import RequestFactory
 class IndexViewTests(TestCase):
     def test_index_view_exits(self):
         """
-        If database is empty, returns empty dict.
+        It returns successful status code when called
         """
         response = self.client.get(reverse('stickerz:index'))
         self.assertEqual(response.status_code, 200)
 
     def test_index_view_categories(self):
+        """
+        It returns empty dict when database is empty.
+        """
         response = self.client.get(reverse('stickerz:index'))
         self.assertQuerysetEqual(response.context['categories'], [])
 
     def test_index_view_one_category(self):
+        """
+        It returns matching category in context dict when database has one non-custom category.
+        """
         # Add sticker to db
         add_sticker('teapot', 'kitchen')
         # get response
@@ -29,6 +34,9 @@ class IndexViewTests(TestCase):
         self.assertContains(response, "kitchen")
 
     def test_index_view_custom_category(self):
+        """
+        It returns empty context dict when database has only custom category.
+        """
         # Add sticker to db
         add_sticker('teapot', 'Custom')
         # get response
@@ -38,21 +46,22 @@ class IndexViewTests(TestCase):
         # test no categories in context dict
         self.assertQuerysetEqual(response.context['categories'], [])
 
-
     def test_index_view_login_link(self):
+        """
+        It returns login link and not dashboard link when user is not authenticated.
+        """
         # setup request factory
         self.factory = RequestFactory()
         # make request
         request = self.factory.get('/index/')
         # set user as logged out user
-        request.user = AnonymousUser()
+        request.user = MockUser()
         # get response
         response= self.client.get( "/stickerz/" )
         # check login link shows
         self.assertContains(response, '<a class="fixed-right" href="%s">' % reverse('stickerz:login'))
         # check dashboard link doesn't show
         self.assertNotContains(response, '<a class="fixed-right" href="%s">' % reverse('stickerz:dashboard'))  
-
 
 """
     Add one sticker for testing
