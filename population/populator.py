ac4e8ff4dec6693dc8be00d85958e87121ebae61
Bella <2770706h@student.gla.ac.uk>
2024-04-21 23:41:41 +0100

    moved verbose into __init__ and encapsulated record creation

--- population/populator.py
+++ population/populator.py
@@ -1,24 +1,28 @@
 class Populator():
-    def __init__(self, reader, models):
+    def __init__(self, reader, models,verbose=True):
         self.models = models # {"table name" : Model,}
         self.data = reader.get_data() # {"table name" : [{"column name" : "column content"}]}
+        self.verbose = verbose
 
-    def populate(self, verbose=True):
-        if verbose:
+    def populate(self):
+        if self.verbose:
             print("# populating started")
         for table_name, model in self.models.items():
-            if verbose:
+            if self.verbose:
                 print("##",table_name)
-            instance = model()
-            table = self.data[table_name]
-            for record in table:
-                for attr_name, attr_content in record.items():
-                    setattr(instance, attr_name, attr_content)
-                if verbose:
-                    print(" -",instance)
-                instance.save()
-        if verbose:
+            self.create_record_from_model(table_name, model)
+        if self.verbose:
             print("population complete")
+
+    def create_record_from_model(self, table_name, model):
+        instance = model()
+        table = self.data[table_name]
+        for record in table:
+            for attr_name, attr_content in record.items():
+                setattr(instance, attr_name, attr_content)
+            if self.verbose:
+                print(" -",instance)
+            instance.save()
                     
 
         
