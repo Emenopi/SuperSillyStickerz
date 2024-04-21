2c30a0d517148f56d5081d67d9ea78d8f8faa1a1
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-04-21 17:34:09 +0100

    free stickers are possible

--- stickerz/sticker_model_test.py
+++ stickerz/sticker_model_test.py
@@ -1,18 +1,47 @@
-from django.test import TestCase, RequestFactory
-from unittest.mock import MagicMock, patch
+from django.test import TestCase
 from stickerz.models import Sticker
-from stickerz.views import sticker
 
 class Sticker_model_tests(TestCase):
-    valid_mock_sticker = Sticker(name="test_1", price=1.00, category="test", sticker_slug="test_1")
-    
-    
-    @patch('stickerz.models.Sticker.save')
-    def test_valid_stickers_get_created(self, valid_mock_sticker):
-        
-        result = Sticker.objects.get_or_create(valid_mock_sticker)
+    def setUp(self):
+        self.valid_sticker_data = {
+            "name": "test 1",
+            "price": 1.00,
+            "category": "test",
+        }
 
-        self.assertTrue(result, True)
+        self.invalid_sticker_data = {
+            "name": "test 2",
+            "price": -1.00,
+            "category": "test",
+        }
+        
+        self.free_sticker_data = {
+            "name": "test 3",
+            "price": 0.00,
+            "category": "test",
+        }
+
+    def test_valid_stickers_get_created(self):
+        sticker, created = Sticker.objects.get_or_create(**self.valid_sticker_data)
+
+        self.assertTrue(created)
+        self.assertEqual(sticker.name, self.valid_sticker_data["name"])
+        self.assertEqual(sticker.price, self.valid_sticker_data["price"])
+        self.assertEqual(sticker.category, self.valid_sticker_data["category"])
+        self.assertEqual(sticker.sticker_slug, "test-1")
+
+
+    def test_invalid_stickers_dont_get_created(self):
+        sticker, created = Sticker.objects.get_or_create(**self.invalid_sticker_data)
+        self.assertFalse(created)
+        #this is currently failing 
+
+
+    def test_free_stickers_can_be_created(self):
+        sticker, created = Sticker.objects.get_or_create(**self.free_sticker_data)
+
+        self.assertTrue(created)
+        self.assertEqual(sticker.price, self.free_sticker_data["price"])
 
 
     
